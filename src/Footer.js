c32c0281a3cb38c3d445002cbbab24c95c5d5aab
Anas-hfd <Anas1.hfd@gmail.com>
2024-07-30 12:56:17 +0100

    HTML and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return <footer>
+    <img src="./Logo.png" alt="Little Lemon logo" />
+    <p>&copy; 2024 Little Lemon</p>
+    </footer>;
+}
+
+export default Footer;
---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Nav() {
+  return <nav>
+      <ul>
+        <li>
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="Menu">Menu</a>
+        </li>
+        <li>
+          <a href="About">About</a>
+        </li>
+      </ul>
+  </nav>;
+}
+
+export default Nav;
